4e91163dfb02576ec922ceba399f3463880ae4e8
Hussain <yunusanusi07@gmail.com>
2021-10-03 22:51:20 +0100

    Integrating with MailChimp API

--- TravelBlog/settings.py
+++ TravelBlog/settings.py
@@ -43,7 +43,7 @@ ALLOWED_HOSTS = ['https://globetrotter-adventures.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
