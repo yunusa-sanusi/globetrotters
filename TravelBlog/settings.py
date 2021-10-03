ca02ae0069a041efd3b6acfc69b25ccf9291cca7
Hussain <yunusanusi07@gmail.com>
2021-10-03 22:54:12 +0100

    Integrating with MailChimp API

--- TravelBlog/settings.py
+++ TravelBlog/settings.py
@@ -34,8 +34,7 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-SECRET_KEY = env(
-    'django-insecure-#4256-h8c6py$mtpg_$#u12p+j2(%idcue+tuxkogd^x1g*kax')
+SECRET_KEY = 'django-insecure-#4256-h8c6py$mtpg_$#u12p+j2(%idcue+tuxkogd^x1g*kax'
 
 
 ALLOWED_HOSTS = ['https://globetrotter-adventures.herokuapp.com', '127.0.0.1']
