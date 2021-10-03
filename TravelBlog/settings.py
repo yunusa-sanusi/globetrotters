23d25e9595bb04ef7b494b7c5bf4177e0071e858
Hussain <yunusanusi07@gmail.com>
2021-10-03 23:13:45 +0100

    Integrating with MailChimp API

--- TravelBlog/settings.py
+++ TravelBlog/settings.py
@@ -37,7 +37,7 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ['https://globetrotter-adventures.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['globetrotter-adventures.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
